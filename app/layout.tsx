478b7e74144f9c66f3197d6a7e5cb451401ef285
Vangling <makara089463923@gmail.com>
2026-04-18 21:33:21 +0700

    feat: add complete favicon and structured data

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,19 @@ export default function RootLayout({
   return (
     <html lang="km">
       <head>
+      ​  {/* Basic favicon */}
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  
+        {/* Apple Touch Icon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  
+        {/* Android Chrome */}
+        <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+  
+        {/* ... rest of your existing head tags ... */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
