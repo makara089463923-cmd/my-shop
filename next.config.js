19fef73cfb7af9702187e2fae014d4f9a1494a5d
Vangling <makara081615512@gmail.com>
2026-08-08 14:16:14 +0700

    update next config to ignore build errors

--- next.config.js
+++ next.config.js
@@ -1,13 +1,38 @@
+// // /** @type {import('next').NextConfig} */
+// // const nextConfig = {
+// //   // Disable preload for CSS to avoid warnings
+// //   experimental: {
+// //     optimizeCss: false,
+// //   },
+// //   // Or configure images if needed
+// //   images: {
+// //     domains: ['localhost'],
+// //     remotePatterns: [
+// //       {
+// //         protocol: 'https',
+// //         hostname: '**',
+// //       },
+// //     ],
+// //   },
+// // }
+
+// // module.exports = nextConfig
+
+
+
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //   // Disable preload for CSS to avoid warnings
 //   experimental: {
 //     optimizeCss: false,
 //   },
-//   // Or configure images if needed
+//   // Configure images with remotePatterns
 //   images: {
-//     domains: ['localhost'],
 //     remotePatterns: [
+//       {
+//         protocol: 'http',
+//         hostname: 'localhost',
+//       },
 //       {
 //         protocol: 'https',
 //         hostname: '**',
@@ -17,28 +42,3 @@
 // }
 
 // module.exports = nextConfig
-
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Disable preload for CSS to avoid warnings
-  experimental: {
-    optimizeCss: false,
-  },
-  // Configure images with remotePatterns
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-      },
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
-  },
-}
-
-module.exports = nextConfig
